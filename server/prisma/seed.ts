b790ae28c835b3294ac33691075b53d2d568c4ce
GFraire <gabrielfreiresa@gmail.com>
2022-11-02 21:28:18 -0300

    Finalizada aula 2

---
+++ server/prisma/seed.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Jonh Doe",
+      email: "jonhdoe@gmail.com",
+      avatarUrl: "https://github.com/GFraire.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Example Pool",
+      code: "BOL123",
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-02T15:30:00.746Z",
+      firstTeamCountryCode: "DE",
+      secondTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-02T15:30:00.746Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "AR",
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();
--- web/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const instance = axios.create({
-  baseURL: "http://0.0.0.0:3333",
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-  },
-});
