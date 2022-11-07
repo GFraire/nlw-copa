1d4a88d0925e3768eb59c64b0516bb4889d4ded5
GFraire <gabrielfreiresa@gmail.com>
2022-11-07 00:13:41 -0300

    Finished app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,29 +1,28 @@
-import { NativeBaseProvider, StatusBar } from "native-base";
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_500Medium,
-  Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
-import { THEME } from "./src/styles/theme";
-import { Loading } from "./src/components/Loading";
-import { SignIn } from "./src/screens/SignIn";
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Pools } from './src/screens/Pools';
+
+import { Loading } from './src/components/Loading';
+
+import { THEME } from './src/styles/theme';
+import { AuthContextProvider } from './src/contexts/AuthContext';
+import { Routes } from './src/routes';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_500Medium,
-    Roboto_700Bold,
-  });
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar 
+          barStyle='light-content'
+          backgroundColor="transparent"
+          translucent
+          />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- server/readme.md
+++ server/readme.md
@@ -1,29 +1,7 @@
-# NLW COPA
+# NLW Copa Backend
 
-Para criar uma migration crie uma .env e coloque nela o codigo:
+Crie uma variável ambiente .env e cole o conteúdo dentro dela:
+`DATABASE_URL="file:./dev.db"`
 
-```javascript
-DATABASE_URL = "file:./dev.db";
-```
-
-Depois rode o comando
-
-```bash
-npx prisma migrate dev
-```
-
-Para abrir o studio do prisma, rode:
-
-```bash
-npx prisma studio
-```
-
-Para gerar uma seed no prisma, rode:
-
-```bash
-npx prisma db seed
-```
-
-## Entidade de Relacionamento
-
-![Tabela de entidade](prisma/ERD.svg)
+Para criar a seed rode no terminal:
+`npx prisma db seed`
